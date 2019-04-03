ceab7fb82f72f17b1339737ef44ecd7ae7fe3437
Hello World Writer <writer@helloworldwriter.com>
2019-04-03 20:35:36 +0300

    Implement conversion buttons functionality

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -1,13 +1,32 @@
 
 
-function doLowercase() {
-    alert("doLowercase");
 
-}
+Case.type('alternate', s => s.replace(/[a-z]/gi,c=>c[`to${(s=!s)?'Low':'Upp'}erCase`]()));
+
+
+const textareaElem = document.getElementById("textarea");
 
 function initButton(id, clickEvent) {
     const buttonElem = document.getElementById(id);
     buttonElem.addEventListener("click", clickEvent, false);
 }
 
-initButton("lowercaseButton", doLowercase);
+function initConvertButton(id, conversion) {
+    initButton(id, () => textareaElem.value = conversion(textareaElem.value));
+}
+
+initConvertButton("lowercase", Case.lower);
+initConvertButton("uppercase", Case.upper);
+initConvertButton("sentenceCase", Case.sentence);
+initConvertButton("titleCase", Case.title);
+
+initConvertButton("alternatingCase", Case.alternate);
+initConvertButton("swapCase", Case.flip);
+initConvertButton("randomCase", Case.random);
+
+initConvertButton("camelCase", Case.camel);
+initConvertButton("pascalCase", Case.pascal);
+initConvertButton("snakeCase", Case.snake);
+initConvertButton("kebabCase", Case.kebab);
+initConvertButton("headerCase", Case.header);
+initConvertButton("constantCase", Case.constant);
